546953bac68d8890ac0ddc8e5babc50568bbcdd5
William Bettridge-Radford <william.bettridge.radford@gmail.com>
2012-12-24 20:39:18 +0000

    end chap 4

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -9,3 +9,65 @@
 // program clears the screen, i.e. writes "white" in every pixel.
 
 // Put your code here.
+
+
+(LOOP)
+    @KBD // A=24576
+    D=M    // D=*A
+    @BLACK
+    D;JGT
+    //WHITE
+    @bit
+    M=0
+    @COLOR_END
+    0;JMP
+
+    (BLACK)
+    @bit
+    M=-1
+
+    (COLOR_END)
+
+    // i=512 {
+        @8192
+        D=A
+        @i
+        M=D
+    // }
+
+    // screen_pointer = SCREEN
+        @SCREEN
+        D=A
+        @screen_pointer
+        M=D
+
+    (FILL_LOOP)
+    // body {
+        @bit
+        D=M
+        @screen_pointer
+        A=M
+        M=D
+    // }
+
+    // step {
+        @i
+        M=M-1
+
+        @screen_pointer
+        M=M+1
+    // }
+
+    // if i > 0 then jump {
+        @i
+        D=M
+        @FILL_LOOP
+        D;JGT
+    // }
+
+    @LOOP
+    0;JMP
+    
+    (END)
+    @END
+    0;JMP
--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -7,3 +7,38 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[3], respectively.)
 
 // Put your code here.
+
+// zero R2
+@R2
+M=0
+
+// i = R0
+@R0
+D=M
+@i
+M=D  // *i=D
+
+(LOOP)
+    // check if loop is over
+    @i   // read R0 into A register
+    D=M   // dereference A into D register
+    @END  // set Jump destination
+    D;JEQ // jump if D == 0
+    
+    // increase R2 by R1
+    @R1   // A=R1
+    D=M   // D=*A
+    @R2   // A=R2
+    M=M+D // *A += D
+
+    // decrement i
+    @i    // A=i
+    M=M-1 // *a -= 1
+
+    @LOOP // set jump destination
+    0;JMP // jump
+
+(END)
+    @END
+    0;JMP
+
