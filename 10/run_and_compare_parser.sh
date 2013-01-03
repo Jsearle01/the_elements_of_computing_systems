aa566ea78ac8724ff654cb0e5b49c5b62f8c5d58
William Bettridge-Radford <william.bettridge.radford@gmail.com>
2013-01-03 10:43:37 +0000

    add; Jack parser test script

---
+++ 10/run_and_compare_parser.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env sh
+
+rm -f test/*/*[^T].xml
+
+for source in test/*/*.jack; do
+    ./JackAnalyzer.py $source
+    out=${source/.jack/.xml}
+    ref=orig/${out#*/*}
+    diff $out $ref > /dev/null
+    if (( $? != 0 )); then
+        echo failed: $source
+    fi
+done
+
